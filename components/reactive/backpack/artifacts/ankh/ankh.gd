38bcaec8bf5f06fa3210e2ca542744ae586bdb22
gruebite <gruebite@gmail.com>
2022-03-09 01:35:16 -0800

    Implement Ankh.

--- components/reactive/backpack/artifacts/ankh/ankh.gd
+++ components/reactive/backpack/artifacts/ankh/ankh.gd
@@ -1,1 +1,17 @@
 extends Artifact
+
+onready var health: Health
+
+func _ready() -> void:
+    if backpack:
+        health = backpack.entity.get_component(Health.NAME)
+
+func _on_out_of_turn() -> void:
+    self.charge += 1
+
+func use(_dir: int) -> bool:
+    assert(health)
+    if health.health == health.max_health:
+        return false
+    health.health += 999
+    return true
