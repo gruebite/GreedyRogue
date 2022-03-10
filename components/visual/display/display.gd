21f776f63217aa208790b8eed6b13f2972509c6d
gruebite <gruebite@gmail.com>
2022-03-10 06:24:38 -0800

    Fix animations.

--- components/visual/display/display.gd
+++ components/visual/display/display.gd
@@ -7,19 +7,20 @@ const NAME := "Display"
 export var background_color := Color.transparent setget set_background_color
 
 export(Brightness.Enum) var brightness: int = Brightness.LIT setget set_brightness
-export var frames: SpriteFrames setget set_frames
-
-export var frame := 0 setget set_frame
-export var playing := false setget set_playing
+export var frames: SpriteFrames setget set_frames, get_frames
+export var playing: bool setget set_playing, get_playing
+export var frame: int setget set_frame, get_frame
 
 export var jitter := true
 
 func _ready() -> void:
+	self.frames = frames
+	self.playing = playing
 	if jitter:
-		if frames.has_animation("dim"):
-			frame = randi() % frames.get_frame_count("dim")
-		elif  frames.has_animation("lit"):
-			frame = randi() % frames.get_frame_count("lit")
+		if self.frames.has_animation("dim"):
+			self.frame = randi() % self.frames.get_frame_count("dim")
+		elif self.frames.has_animation("lit"):
+			self.frame = randi() % self.frames.get_frame_count("lit")
 
 func set_background_color(c: Color) -> void:
 	background_color = c
@@ -36,32 +37,38 @@ func set_brightness(to: int) -> void:
 			show()
 			if $Foreground.frames.has_animation("lit"):
 				$Foreground.show()
+				var f = $Foreground.frame
 				$Foreground.animation = "lit"
-				$Foreground.frame = playing
-				$Foreground.frame = frame
+				$Foreground.frame = f
 			else:
 				$Foreground.hide()
 		Brightness.DIM:
 			show()
 			if $Foreground.frames.has_animation("dim"):
 				$Foreground.show()
+				var f = $Foreground.frame
 				$Foreground.animation = "dim"
-				$Foreground.frame = playing
-				$Foreground.frame = frame
+				$Foreground.frame = f
 			else:
 				$Foreground.hide()
 
 func set_frames(fs: SpriteFrames) -> void:
 	frames = fs
-	if not is_inside_tree(): yield(self, 'ready')
 	$Foreground.frames = fs
+
+func get_frames() -> SpriteFrames:
+	return $Foreground.frames
 
 func set_frame(f: int) -> void:
 	frame = f
-	if not is_inside_tree(): yield(self, 'ready')
 	$Foreground.frame = f
+
+func get_frame() -> int:
+	return $Foreground.frame
 
 func set_playing(p: bool) -> void:
 	playing = p
-	if not is_inside_tree(): yield(self, 'ready')
 	$Foreground.playing = p
+
+func get_playing() -> bool:
+	return $Foreground.playing
